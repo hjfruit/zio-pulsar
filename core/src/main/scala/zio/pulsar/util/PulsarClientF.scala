2ffd4cd7bdb5645053da45c235f43f046a69d213
liguobin <liguobin@hjfruit.com>
2023-04-21 16:02:38 +0800

    fix scope

--- core/src/main/scala/zio/pulsar/util/PulsarClientF.scala
+++ core/src/main/scala/zio/pulsar/util/PulsarClientF.scala
@@ -15,28 +15,28 @@ object PulsarClientF:
     topic: String,
     subscription: Subscription[K],
     schema: Schema[M]
-  ): ZIO[PulsarClient & Scope, PulsarClientException, Consumer[M]] =
-    for
+  ): ZIO[PulsarClient, PulsarClientException, Consumer[M]] =
+    (for
       builder <- ConsumerBuilder.make(schema)
       cr      <- builder
                    .topic(topic)
                    .subscription(subscription)
                    .build
-    yield cr
+    yield cr).provideSomeLayer(ZLayer.fromZIO(Scope.make))
 
   def consumeF[K <: SubscriptionKind](
     topic: String,
     subscription: Subscription[K]
-  ): ZIO[PulsarClient & Scope, PulsarClientException, Consumer[String]] =
+  ): ZIO[PulsarClient, PulsarClientException, Consumer[String]] =
     consumeF(topic, subscription, Schema.STRING)
 
-  def productF(topic: String): ZIO[PulsarClient & Scope, PulsarClientException, Producer[String]] =
+  def productF(topic: String): ZIO[PulsarClient, PulsarClientException, Producer[String]] =
     productF(topic, Schema.STRING)
 
-  def productF[M](topic: String, schema: Schema[M]): ZIO[PulsarClient & Scope, PulsarClientException, Producer[M]] =
-    for
+  def productF[M](topic: String, schema: Schema[M]): ZIO[PulsarClient, PulsarClientException, Producer[M]] =
+    (for
       builder <- ProducerBuilder.make(schema)
       pd      <- builder
                    .topic(topic)
                    .build
-    yield pd
+    yield pd).provideSomeLayer(ZLayer.fromZIO(Scope.make))
--- version.sbt
+++ version.sbt
@@ -1,1 +1,1 @@
-ThisBuild / version := s"0.3.0-${git.gitHeadCommit.value.map(_.substring(0, 8)).get}-SNAPSHOT"
+ThisBuild / version := s"0.3.1-${git.gitHeadCommit.value.map(_.substring(0, 8)).get}-SNAPSHOT"
